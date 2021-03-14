f4c3a0496baf5799b87523d9b3b59f593c5af878
Radoslav Backovsky <radoslav.backovsky@gmail.com>
2021-03-14 16:28:48 +0100

    edit sql

--- db/V1__Workout_table_and_WorkoutTypes_table.sql
+++ db/V1__Workout_table_and_WorkoutTypes_table.sql
@@ -1,11 +1,11 @@
-CREATE TABLE Workouts(
-    Id INT PRIMARY KEY IDENTITY (1, 1),
+CREATE TABLE Workouter.dbo.Workouts(
+    Id INT PRIMARY KEY IDENTITY,
     TypeId INT NOT NULL,
     Distance FLOAT NOT NULL
 );
 
 
-CREATE TABLE WorkoutTypes(
-    Id INT PRIMARY KEY IDENTITY (1, 1),
+CREATE TABLE Workouter.dbo.WorkoutTypes(
+    Id INT PRIMARY KEY IDENTITY,
     Name VARCHAR (50) NOT NULL
 );
